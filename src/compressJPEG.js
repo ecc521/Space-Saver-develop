c938440fb02c4fcb869764b70ed616ca723444c8
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-08 08:13:51 -0500

    Update compressJPEG.js

--- src/compressJPEG.js
+++ src/compressJPEG.js
@@ -1,1 +1,17 @@
+'use strict'
 
+const fs = require("fs")
+
+const { spawn } = require('child_process');
+
+
+const jpegtran = "./mozjpeg-build/jpegtran-static"
+
+
+
+//Working on the function offline
+
+
+
+
+module.exports.compressJPEG = compressJPEG
