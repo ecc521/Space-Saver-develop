6eff7538c4738c5e08cda6e286898b1324beab3c
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-08 10:46:15 -0500

    Update transparentCompression.js

--- src/transparentCompression.js
+++ src/transparentCompression.js
@@ -4,6 +4,7 @@
 //Different operating systems have different transparent filesystem compression capabilities.
 //Currently only Windows and macOS are supported (Linux doesn't have built in filesystem compression)
 
+//Make sure to add a way to undo transparent compression - this shouldn't be too hard
 
 //Returns win32 even on 64 bit platforms - likely will never be changed due to backwards compatability
 if (process.platform === "win32") {
