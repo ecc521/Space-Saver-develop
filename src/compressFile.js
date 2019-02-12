4370e5bb0ea6546deb5e73570cc723d3ff945eaf
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-12 10:48:55 -0500

    Create compressFile.js

---
+++ src/compressFile.js
@@ -0,0 +1,21 @@
+let uncompressable = []
+
+//Movie formats
+uncompressable.concat(["mp4"])
+
+//Already Compressed Files
+uncompressable.conact(["xz", "txz", "lzma", "bz", "bz2", "tbz", "gz", "tgz", "lz"])
+
+
+
+
+function compressFile(src) {
+
+
+
+
+
+
+
+
+}
