bb8a72fe4c4c942a2cffe4a4b1ca05817ab417bd
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-12 15:10:23 -0500

    Add files via upload

--- src/uncompressable.js
+++ src/uncompressable.js
@@ -1,21 +1,41 @@
 
-//Files that will not benefit from compression
+//Files that will not benefit from transparent filesystem compression
 //Note: If there is an algorithm for recompressing the file effectively, this list will be ignored
 
-//These should be ordered alphabetically
+//The list (uncompressable.txt) should be sorted alphabetically
 
-let uncompressable = [
-	".mp4",
-	".xz",
-	".txz",
-	".lzma",
-	".bz",
-	".bz2",
-	".tbz",
-	".gz",
-	".tgz",
-	".lz",
-]
+
+const fs = require("fs")
+
+
+let fileText = fs.readFileSync("uncompressable.txt", "utf8")
+let fileLines = fileText.split("\n")
+
+let extensions = []
+
+//Collect all the file extensions. Ignore comments
+for (let i=0;i<fileLines.length;i++) {
+	
+	let line = fileLines[i]
+	
+	if (line.startsWith("//") || line.length === 0) {
+		continue;
+	}
+	
+	let arr = line.split(" ")
+	let extension = arr[0]
+	
+	//Add the dot if the user didn't
+	if (extension.startsWith(".")) {
+		extensions.push(extension)
+	}
+	else {
+		extensions.push("." + extension)
+	}
+}
+
+
+
 
 
 
