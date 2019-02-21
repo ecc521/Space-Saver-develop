aaf3f8fec910448d86db94d387b3fbb930f7cdb2
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-21 16:48:46 -0500

    Add files via upload

---
+++ src/calculateFiles.js
@@ -0,0 +1,29 @@
+function filterExcludedPaths(paths) {
+    
+    //The longer paths will be more specific
+    paths.sort((a,b) => {return a.length - b.length})
+    
+    //Now we need to go into 
+    
+    
+    
+}
+
+
+
+
+function getSelectedPaths() {
+    let elems = document.querySelectorAll(".selectedItems")
+    let pathArray = []
+    for (let i=0;i<elems.length;i++) {
+        let elem = elems[i]
+        pathArray.push({
+            //className location
+            //className mode
+        })
+    }
+    
+    
+}
+
+
