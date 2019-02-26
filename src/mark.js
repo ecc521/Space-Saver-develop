f9e27c53a7e951b16d6bf82a377da0041477996b
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-02-26 17:38:02 -0500

    macOS Don't Re-compress Files
    Untested. May not work

--- src/mark.js
+++ src/mark.js
@@ -3,24 +3,57 @@ const { spawn } = require("child_process")
 
 if (process.platform === "darwin") {
     
+	let attributeName = "com.spacesaver.lastcompressed"
+	
     //Use xattr
+    async function isMarked(src) {
+		
+		let lastModified = fs.statSync(src).mtimeMs //Get last modified time
+		//Get the time the file was last compressed from xattr value
+        let lastCompressed = await new Promise((resolve, reject) => {
+			let reader = spawn("xattr", ["-p", attributeName, src], {
+				detached: true,
+				stdio: [ 'ignore', "pipe", "pipe" ]
+			})
+			
+			reader.stdout.on("data", resolve)
+			reader.stderr.on("data", reject)
+			reader.on("close", resolve)
+			//Make sure to handle no timestamp
+			//Make sure this behaves correctly
+				
+		})		
+	
+		return lastCompressed > lastModified //true if the file has been compressed since it's last modification		
+    }
+
+	
     function markFile(src) {
-        let lastModified = 0 //Get last modified date
-        let lastCompressed = 0 //Get last compressed date from xattr value
 		
-		
-		
+		return new Promise((resolve, reject) => {
+			//Set attributeName to the current time
+			let marker = spawn("xattr", ["-w", attributeName, Date.now(),src], {
+				detached: true,
+				stdio: [ 'ignore', "pipe", "pipe" ]
+			})
+			marker.stdout.on("data", resolve)
+			marker.stderr.on("data", reject)
+			marker.on("close", resolve)
+		})
     }
+} //End of code for darwin
 
-    function isMarked(src) {
-
-    }
-}
+//Need to figure out what do do for Windows and Linux
 else {
     //Bogus functions for now
     function isMarked(src) {return false}
     function markFile(src) {}
 }
+
+
+
+
+
 
 
 module.exports = {
