a20ff25c4a6a064654a355627593ae7282fff647
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-03-02 21:46:00 -0500

    Build Improvements
    Now builds for rpm targets
    Build process paralellized better
    Version update

--- build.sh
+++ build.sh
@@ -1,46 +1,62 @@
 
-echo "The first linux build will likely error - the second one should succeed"
-
-npm install
-
-#yarn errors otherwise
-rm package-lock.json
+#May need to run brew install rpm
+#May need to run npm install
 
 
-#For somer reason rpm targets are erroring
-electron-forge make --arch=ia32,x64,armv7l,arm64 --platform=linux --targets=deb
+#May need to delete package-lock.json (only if it fails first time)
 
 
-#Bypass issues where it gets the name and productName mixed up
+
+#Didn't see a reason to build zip targets
+
+#rpmbuild is SINGLE THREADED!!! (So it takes forever)
+#It is run at high priority so it the other builds don't slow it down
+
+
+{
+electron-forge package --platform=linux --arch=x64
+
+#Bypass issues where name and productName get mixed up
+#It looks for an executable named space-saver when the executable is named Space Saver. This creates symbolic links to the actual executable so it works.
 cd out
-
 cd Space\ Saver-linux-x64
 ln -fs Space\ Saver space-saver
 cd ../
-
-cd Space\ Saver-linux-ia32
-ln -fs Space\ Saver space-saver
 cd ../
 
-cd Space\ Saver-linux-armv7l
-ln -fs Space\ Saver space-saver
-cd ../
+electron-forge make --skip-package --arch=x64 --platform=linux --targets=deb & 
+nice -10 electron-forge make --skip-package --arch=x64 --platform=linux --targets=rpm
+}&
+{
+sleep 0.1
+electron-forge package --platform=linux --arch=arm64
 
+#Bypass issues where name and productName get mixed up
+#It looks for an executable named space-saver when the executable is named Space Saver. This creates symbolic links to the actual executable so it works.
+cd out
 cd Space\ Saver-linux-arm64
 ln -fs Space\ Saver space-saver
 cd ../
-
-
 cd ../
 
-electron-forge make --skip-package --arch=ia32,x64,armv7l,arm64 --platform=linux --targets=deb
-
-
-
-
-
-electron-forge make --arch=x64 --platform=darwin --targets=dmg #Didn't see a reason to build zip target, but it works
-
-electron-forge make --arch=arm64 --platform=win32 --targets=squirrel
+electron-forge make --skip-package --arch=arm64 --platform=linux --targets=deb & 
+nice -10 electron-forge make --skip-package --arch=arm64 --platform=linux --targets=rpm
+}&
+{
+sleep 0.2
+electron-forge make --arch=x64 --platform=darwin --targets=dmg
+}&
+{
+sleep 0.3
 electron-forge make --arch=x64 --platform=win32 --targets=squirrel
-electron-forge make --arch=ia32 --platform=win32 --targets=squirrel
+}
+
+
+
+
+
+
+
+
+
+
