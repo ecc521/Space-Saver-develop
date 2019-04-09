60ae38070202c78bcb0730d5d2a6e8d70db0b25e
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-04-09 18:30:46 -0400

    Update compressJPEG.js
    Allow for undoing JPEG compression

--- src/compressJPEG.js
+++ src/compressJPEG.js
@@ -9,20 +9,17 @@ const binarySelector = require("./binarySelector.js")
 const jpegtranPath = binarySelector.getBinaryPath("jpegtran")
 
 
-async function compressJPEG(inputSrc) {	
+
+async function jpegtran(inputSrc, parameters, forceOverwrite) {	
     
+	parameters.push(inputSrc)
+	
 	let stdoutCache = [] //Buffers to write into file cached are stored here
 
 	//Compress jpeg here. Throw error if something goes wrong
 	await new Promise((resolve, reject) => {
 				        
-        //Pass -copy all in order to avoid issues where the rotation flag is not copied.
-        //When that happens, the image shows up flipped
-        
-        //If the -arithmetic flag is used instead of -optimize, compression almost doubles
-        //However few applications support arithmetic coded JPEG's
-        //Although jpegtran supports arithmetic coded JPEG's, the specific build may not.
-		let compressor = spawn(jpegtranPath, ["-optimize", "-progressive", "-copy", "all", inputSrc], {
+		let compressor = spawn(jpegtranPath, parameters, {
 			detached: true,
 			stdio: [ 'ignore', "pipe", "pipe" ]
 		})
@@ -43,17 +40,16 @@ async function compressJPEG(inputSrc) {
 		
 	})
 		
-	//Let's just make sure that the output file has a smaller file size
-	
+	//Calculate the file sizes
 	let originalSize = fs.statSync(inputSrc).size
 	let compressedSize = 0
 	
 	for (let i=0;i<stdoutCache.length;i++) {
 		compressedSize += stdoutCache[i].length
 	}
-	
-	
-	if (originalSize > compressedSize) {
+		
+	//Let's just make sure that the output file has a smaller file size
+	if (originalSize > compressedSize || forceOverwrite) {
 		//Overwrite the original file with the compressed data
 		fs.unlinkSync(inputSrc)
 		let outputFile = fs.openSync(inputSrc, "a") //The file no longer exists, so it will be created here
@@ -67,10 +63,41 @@ async function compressJPEG(inputSrc) {
 	return {
 		originalSize,
 		compressedSize, 
-        mark: true,
 	}
 	
 }
 
 
-module.exports.compressJPEG = compressJPEG
+//Pass -copy all in order to avoid issues where the rotation flag is not copied.
+//When that happens, the image shows up flipped        
+
+async function compressJPEG(src) {
+	
+    //If the -arithmetic flag is used instead of -optimize, compression almost doubles (and I believe becomes faster)
+    //However few applications support arithmetic coded JPEG's
+    //Although jpegtran supports arithmetic coded JPEG's, the specific build may not.
+	
+	let result = await jpegtran(src, ["-optimize", "-progressive", "-copy", "all"])
+	result.mark = true
+	return result
+}
+
+async function toBaselineJPEG(src) {
+	let result = await jpegtran(src, ["-revert", "-copy", "all"], true) //Force overwrite
+	result.mark = false
+	return result
+}
+
+
+function revertJPEG(...parameters) {
+	//May want to detect if the original JPEG was progressive, ext.
+	return toBaselineJPEG(...parameters)
+}
+
+
+module.exports = {
+	jpegtran,
+	compressJPEG,
+	toBaselineJPEG,
+	revertJPEG,
+}
