89d456140e526486f6e1bb8c4c5f5cdc77260ad9
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-04-10 11:13:29 -0400

    Update setPriority.js

--- src/setPriority.js
+++ src/setPriority.js
@@ -1,6 +1,17 @@
 const {spawnSync} = require("child_process")
 
+/*
+wmic process where name="calc.exe" CALL setpriority "idle"
 
+Either the word in quotes or the number can be used
+
+Idle:		64
+Below Normal:	16384 
+Normal:		32 
+Above Normal:	32768
+High Priority:	128
+Real Time:	256
+*/
 function windowsSetPriority() {
 
 }
