6997ac5fcd9cb07d80b9de2957cf6cd179989426
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-04-10 11:02:51 -0400

    Update setPriority.js

--- src/setPriority.js
+++ src/setPriority.js
@@ -1,1 +1,15 @@
+const {spawnSync} = require("child_process")
+
+function windowsSetPriority() {
+
+}
+
 //renice -n nicenessvalue -p processid
+function setPriority(pid, priority) {
+  priority = priority||20 //Use minimum priority if not set
+  spawnSync("renice", ["-n", priority, "-p", pid])
+}
+
+module.exports = {
+
+}
