731ffb8ddc53a9e4add53a72ddcde8cf14db5f42
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-04-16 12:43:32 -0400

    Minor Changes, Mostly UI
    Alerts user when they press shrink while compressing
    No longer alerts when you press shrink while a compression operation is already in progress.
    Made numbers smaller (rounded, bytes => megabytes)
    Should make compression starting immediantly obvious (it did in my testing.)

--- src/renderer.js
+++ src/renderer.js
@@ -56,6 +56,7 @@ document.getElementById("menu").appendChild(pauseButton)
 let resumeButton = document.createElement("button")
 resumeButton.innerHTML = "Resume" //Need to resize button. "Resume Compression" went onto a second line
 resumeButton.addEventListener("click", function(){
+    ipcRenderer.send('asynchronous-message', false) //The window can no longer be terminated
     scheduler.resumeCompression()
     resumeButton.replaceWith(pauseButton)
 })
@@ -67,42 +68,43 @@ resumeButton.className = "btn"
 
 
 function update() {
-    document.getElementById("progressBarOuter").style.display = "block"
     document.getElementById("progressBarInner").style.width = (compressed/count*100) + "%"
-    progressText.innerHTML = `${compressed}/${count} files have been compressed at a savings of ${savings} bytes (${savings/totalSize*100}% reduction)`
+    //Sorry - I'm using megabytes instead of mebibytes, because some people may not know what MiB is.
+    progressText.innerHTML = `${compressed}/${count} files compressed. ${(savings/1000000).toFixed(2)} MB saved out of ${(totalSize/1000000).toFixed(2)} MB (${(savings/totalSize*100).toFixed(2)}% reduction)`
 }
 
 
 function reduceStorageSpace() {
     let schedulerStatus = scheduler.getLocalValues()
-    
+
     if (schedulerStatus.paused) {
-        if (!confirm("Compression on some files is currently paused. Would you like to stop that compression, and start shrinking the files you have selected? (adding additional files is not yet supported - it results in duplicate compressions)")) {
-            return;
-        }
-        else {
-            scheduler.clearCompressionQueue()
-            scheduler.resumeCompression()
-            resumeButton.replaceWith(pauseButton)
-        }
+        //Don't bother alerting the user - file marking should prevent wasting of CPU time.
+        scheduler.clearCompressionQueue()
+        scheduler.resumeCompression()
+        resumeButton.replaceWith(pauseButton)
     }
-    
-    
+    else if (schedulerStatus.compressionQueue.length > 0) {
+        alert("Compression is currently in progress. Please pause compression before adding more files.")
+        return;
+    }
+
+
     ipcRenderer.send('asynchronous-message', false) //The window can no longer be terminated
 
+    document.getElementById("progressBarOuter").style.display = "block"
     progressText.innerHTML = "Beginning compression... It may take a few seconds before results show"
-    
+
     let paths = calculateFiles.getSelectedPaths()
     let filteredPaths = calculateFiles.filterExcludedPaths(paths)
-    
+
     //Run compression on all files in filteredPaths.
-    
+
     let start = Date.now()
     savings = 0
     totalSize = 0
     compressed = 0
     count = 0
-        
+
     for (let path in filteredPaths) {
         count++
         scheduler.compressParalell(path).then((results) => {
@@ -110,21 +112,21 @@ function reduceStorageSpace() {
             console.log(Date.now() - start)
             console.log(path)
             console.log(results)
-            
+
             let fileSavings = results.originalSize - results.compressedSize
             if (!isNaN(fileSavings)) {
                 savings += fileSavings
                 totalSize += results.originalSize
             }
             compressed++
-            
+
             update()
             if (compressed === count) {
                 ipcRenderer.send('asynchronous-message', true) //The window can now be terminated
             }
         })
     }
-    
+
 }
 
 
@@ -142,14 +144,14 @@ if (require("electron").remote.systemPreferences.isDarkMode()) {
     let elem = document.createElement("style")
     elem.innerHTML = `
 body {
-    background: black;
-    color: white;
+background: black;
+color: white;
 }
 .selectedItem {
-    color:black
+color:black
 }
 h1 {
-    text-align:center;
+text-align:center;
 }
 `
     document.body.appendChild(elem)
@@ -161,7 +163,7 @@ h1 {
 //All this is for rendering the select folder and select file buttons
 
 let paths = [];
- 
+
 
 let selectedItems = document.querySelector('#selectedItems')
 let selectedItemsHeader = document.querySelector('#selectedItemsHeader')
@@ -171,125 +173,125 @@ let selectedItemsHeader = document.querySelector('#selectedItemsHeader')
 //Create Add Files button
 //If a single file dialog for both folders and files is not supported, also make an add folders button
 function createButton(text) {
-	let button = document.createElement("button")
-	button.className = "btn"
-	button.innerText = text
-	return button
+    let button = document.createElement("button")
+    button.className = "btn"
+    button.innerText = text
+    return button
 }
 
 
 let menu = document.getElementById("menu")
 
 if (process.platform === "darwin") {
-	//Create one button for both file and folder selection
-	let selectButton = createButton("📂 Select Files")
-	selectButton.addEventListener("click", function(){addPaths(true, true)})
-	menu.appendChild(selectButton)
+    //Create one button for both file and folder selection
+    let selectButton = createButton("📂 Select Files")
+    selectButton.addEventListener("click", function(){addPaths(true, true)})
+    menu.appendChild(selectButton)
 }
 else {
-	//Create a button for file selection and a button for folder selection
-	let folderButton = createButton("📂 Select Folders")
-	folderButton.addEventListener("click", function(){addPaths(false, true)})
-	menu.appendChild(folderButton)
-	
-	let fileButton = createButton("Select Files")
-	fileButton.addEventListener("click", function(){addPaths(true, false)})
-	menu.appendChild(fileButton)
+    //Create a button for file selection and a button for folder selection
+    let folderButton = createButton("📂 Select Folders")
+    folderButton.addEventListener("click", function(){addPaths(false, true)})
+    menu.appendChild(folderButton)
+
+    let fileButton = createButton("Select Files")
+    fileButton.addEventListener("click", function(){addPaths(true, false)})
+    menu.appendChild(fileButton)
 }
 
 
 
 function addPaths(files, folders){
-	let properties = []
-	
-	//On Windows and Linux a single dialog can't be for both folders and directories. Electron shows a file selector
-	
-//Create both an Add Folders and an Add Files
-	if (folders) {
-		properties.push("openDirectory")
-	}
-	
-	if (files) {
-		properties.push("openFile")
-	}
-	
-	properties.push("multiSelections") //Allow selecting multiple files
-	
- let options = {     
-  properties,
- }
- 
- dialog.showOpenDialog(options, (newPaths) => {
-     if (!newPaths) {
-         return;
-     }
-     
-  newPaths.forEach(newPath =>{
-      let exist = paths.some((pathObject) => {
-          return pathObject.path == newPath
-      })
+    let properties = []
 
-      if (exist){
-          //alert(newPath + " has already been selected.") 
-          return;
-      }
+    //On Windows and Linux a single dialog can't be for both folders and directories. Electron shows a file selector
 
-      new pathObject(newPath);
-  })
- })
+    //Create both an Add Folders and an Add Files
+    if (folders) {
+        properties.push("openDirectory")
+    }
+
+    if (files) {
+        properties.push("openFile")
+    }
+
+    properties.push("multiSelections") //Allow selecting multiple files
+
+    let options = {     
+        properties,
+    }
+
+    dialog.showOpenDialog(options, (newPaths) => {
+        if (!newPaths) {
+            return;
+        }
+
+        newPaths.forEach(newPath =>{
+            let exist = paths.some((pathObject) => {
+                return pathObject.path == newPath
+            })
+
+            if (exist){
+                //alert(newPath + " has already been selected.") 
+                return;
+            }
+
+            new pathObject(newPath);
+        })
+    })
 }
 
 
 function pathObject (newPath){
- this.path = newPath
- this.isIncluded = true
- paths.push (this)
- 
- selectedItemsHeader.className = ''
+    this.path = newPath
+    this.isIncluded = true
+    paths.push (this)
 
- let textNode = document.createElement("p")
- textNode.innerText = newPath
+    selectedItemsHeader.className = ''
+
+    let textNode = document.createElement("p")
+    textNode.innerText = newPath
     textNode.style.display = "inline-block"
     textNode.className = "location"
-    
-    
- let del = document.createElement('span')
- del.className = 'delDir';
- del.innerHTML = '✖'
- 
- let type = document.createElement('span');
- type.className = 'mode'
- type.innerHTML = 'included';
- 
- let div = document.createElement('div')
- div.className = "selectedItem"
- div.appendChild(del)
- div.appendChild(type)
- div.appendChild(textNode)
- selectedItems.appendChild(div)
- 
- del.addEventListener('click', () => {
-  selectedItems.removeChild(div)
-  let index = paths.indexOf(this)
 
-  if (index !== -1)
-   paths.splice(index, 1)
 
-  if (paths.length === 0 )
-   selectedItemsHeader.className = 'hide';
- })
- 
- type.addEventListener('click', () => {
-  this.isIncluded = !this.isIncluded
-  if (this.isIncluded){
-   type.className = 'mode'
-   type.innerHTML = 'included'
-  } 
-     else {
-   type.className = 'mode modeExcluded'
-   type.innerHTML = 'excluded'
-  }
- })
+    let del = document.createElement('span')
+    del.className = 'delDir';
+    del.innerHTML = '✖'
+
+    let type = document.createElement('span');
+    type.className = 'mode'
+    type.innerHTML = 'included';
+
+    let div = document.createElement('div')
+    div.className = "selectedItem"
+    div.appendChild(del)
+    div.appendChild(type)
+    div.appendChild(textNode)
+    selectedItems.appendChild(div)
+
+    del.addEventListener('click', () => {
+        selectedItems.removeChild(div)
+        let index = paths.indexOf(this)
+
+        if (index !== -1)
+            paths.splice(index, 1)
+
+        if (paths.length === 0 )
+            selectedItemsHeader.className = 'hide';
+    })
+
+    type.addEventListener('click', () => {
+        this.isIncluded = !this.isIncluded
+        if (this.isIncluded){
+            type.className = 'mode'
+            type.innerHTML = 'included'
+        } 
+        else {
+            type.className = 'mode modeExcluded'
+            type.innerHTML = 'excluded'
+        }
+    })
 }
 
 
