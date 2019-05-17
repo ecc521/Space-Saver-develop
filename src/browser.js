503ab8c6bfcaf663988a7ae10819a16a184e797d
ecc521 <42424091+ecc521@users.noreply.github.com>
2019-05-17 15:15:12 -0400

    Add Reload Button. Add FLAC to package.json

--- src/browser.js
+++ src/browser.js
@@ -1,6 +1,7 @@
 require("./allPages.js")
 
 
+
 function createSearchLink(query) {
 	return "https://www.google.com/search?q=" + query
 }
@@ -8,10 +9,35 @@ function createSearchLink(query) {
 let homePage = "https://www.google.com/"
 
 
+let view = document.createElement("webview")
+view.style.width = "100%"
+view.style.height = "calc(100% - 30px)"
+window.addEventListener("DOMContentLoaded", function() {
+	view.allowfullscreen = true
+	view.enableremotemodule = false
+	view.src = homePage
+})
+
+
+
+let backButton = document.createElement("button")
+backButton.className = "navigationButton"
+backButton.innerHTML = "⬅"
+
+let forwardButton = document.createElement("button")
+forwardButton.className = "navigationButton"
+forwardButton.innerHTML = "➡"
+
+let reloadButton = document.createElement("button")
+reloadButton.className = "navigationButton"
+reloadButton.innerHTML = "↻"
+
+
 let urlBar = document.createElement("input")
 urlBar.id = "urlBar"
 urlBar.placeholder = "Type URL or Search Query Here..."
-document.body.appendChild(urlBar)
+
+
 
 urlBar.addEventListener("keydown", function(event){
 	if (event.key === "Enter") {
@@ -32,23 +58,28 @@ urlBar.addEventListener("keydown", function(event){
 	}
 })
 
-
-let view = document.createElement("webview")
-view.style.width = "100%"
-view.style.height = "calc(100% - 30px)"
-document.body.appendChild(view)
-window.addEventListener("DOMContentLoaded", function() {
-	view.allowfullscreen = true
-	view.enableremotemodule = false
-	view.src = homePage
+backButton.addEventListener("click", function() {
+	view.goBack()
 })
 
+forwardButton.addEventListener("click", function() {
+	view.goForward()
+})
 
+reloadButton.addEventListener("click", function() {
+	view.reload()
+})
 
 view.addEventListener("did-start-loading", function() {
-	urlBar.value = view.getURL()
+	urlBar.value = view.src
 })
 
 view.addEventListener("did-finish-load", function() {
 	urlBar.value = view.getURL()
 })
+
+document.body.appendChild(backButton)
+document.body.appendChild(forwardButton)
+document.body.appendChild(reloadButton)
+document.body.appendChild(urlBar)
+document.body.appendChild(view)
